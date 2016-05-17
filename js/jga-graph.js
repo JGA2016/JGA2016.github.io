a10e06df30fec00f2d5b6d304d724928297852ad
Paul B <paul.beaujean@orange.com>
2016-05-17 15:25:14 +0200

    Rename svg selector

--- js/jga-graph.js
+++ js/jga-graph.js
@@ -2,29 +2,29 @@ var width = 2000;
 var height = 600;
 var step = 100;
 
-var lines = d3.select("svg")
+var svg = d3.select("svg")
 
 for (var x = 0; x <= width; x += step) {
-    lines.append("line")
+    svg.append("line")
 	.attr("x1", x).attr("y1", 0)
 	.attr("x2", x).attr("y2", height)
 	.attr("class", "link")
 }
 for (var y = 0; y <= height; y += step) {
-    lines.append("line")
+    svg.append("line")
 	.attr("x1", 0).attr("y1", y)
 	.attr("x2", width).attr("y2", y)
 	.attr("class", "link")
 }
 
 for (var x = 0; x <= width; x += step) {
-    nodes.append("circle")
+    svg.append("circle")
 	.attr("r", 20)
 	.attr("cx", x).attr("cy", 0)
 	.attr("class", "node")
 }
 for (var y = 0; y <= height; y += step) {
-    nodes.append("circle")
+    svg.append("circle")
 	.attr("r", 20)
 	.attr("cx", 0).attr("cy", y)
 	.attr("class", "node")
