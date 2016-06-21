ba8f280744b64f6a69a988ab904bf7c18b0ec8c5
Paul B <paul.beaujean@orange.com>
2016-06-21 16:52:45 +0200

    Draw at the right coordinates

--- js/jga-graph.js
+++ js/jga-graph.js
@@ -24,17 +24,17 @@ var scaling = 100;
 
 var letterJ = scale(scaling, 100, 100, [[0,0],[2,0],[4,0],[4,1.5],[4,3],[2,4],[0,3]]);
 var letterG = scale(scaling, 100 + 600, 100, [[2,0],[0,2],[2,4],[4,3],[2,2]]);
-var petersenOuterCycle = scale(scaling, 100, 100 + 600 + 600,
+var petersenOuterCycle = scale(scaling, 100 + 600 + 600, 100,
 			       [[2,0],[3.5,2],[4,5],[0,5],[0.5,2],[2,0]]);
-var petersenInnerStar = scale(scaling, 100, 100 + 600 + 600, 
+var petersenInnerStar = scale(scaling, 100 + 600 + 600, 100, 
 			[[2,1.5],[2.2,4],[1.5,2.5],[2.5,2.5],[1.8,4],[2,1.5]]);
-var petersenConnectors = scale(scaling, 100, 100 + 600 + 600,
+var petersenConnectors = scale(scaling, 100 + 600 + 600, 100,
 			 [[2,0],[2,1.5],
 			  [3.5,2],[2.5,2.5],
 			  [4,5],[2.2,4],
 			  [0,5],[1.8,4],
 			  [0.5,2],[1.5,2.5]]);
-var petersenNodes = scale(scaling, 100, 100 + 600 + 600,
+var petersenNodes = scale(scaling, 100 + 600 + 600, 100,
 			  [[2,0],[3.5,2],[4,5],[0,5],[0.5,2],
 			   [2,1.5],[2.2,4],[1.5,2.5],[2.5,2.5],[1.8,4]]);
 console.log(petersenOuterCycle, petersenInnerStar, petersenConnectors)
